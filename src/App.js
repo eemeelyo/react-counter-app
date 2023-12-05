a738c54720c993a95eaf2540d29b185da4cfcda8
eemeelyo <ecomiling@rocketmail.com>
2023-12-05 07:35:58 +0000

    Counter App commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  let color = "#fff";
+  if (count < 0) {
+    color = "red";
+  } else if (count > 0) {
+    color = "yellow";
+  } else {
+    color = "white";
+  }
+
   return (
-    <div className="App">
-      <h1>Hello eemeelyo!</h1>
-    </div>
+      <section className="counter-sec --flex-center">
+        <div className="container counter --card --center-all">
+          <h1 className="--text-light">React Counter App</h1>
+          <p className='count --my2 --text-md --fw-bold' style={{color: color}}>{count}</p>
+          <div className="buttons">
+            <button className='--btn --btn-danger' onClick={handleSubtract}>- Subtract</button>
+            <button className='--btn' onClick={handleReset}>! Reset</button>
+            <button className='--btn --btn-success' onClick={handleAdd}>+ Add</button>
+          </div>
+        </div>
+      </section>
   );
 }
 
